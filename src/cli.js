31bcb2cc42fafc89efe5b7ee492baf6b1a6f7856
zebpaa <skvorcov_90@mail.ru>
2024-06-17 11:32:07 +0400

    fixed the output

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 export default () => {
-  const name = readlineSync.question('May I have your name? ');
+  console.log("Welcome to the Brain Games!");
 
-  if (name) {
-    console.log(`Hello, ${name}!`);
-  };
+  const name = readlineSync.question("May I have your name? ");
+
+  console.log(`Hello, ${name}!`);
 };
