efbeb9f0dbaba55f3bf775a1844f2368b67f1ef5
zebpaa <skvorcov_90@mail.ru>
2024-06-20 17:00:45 +0400

    corrected the description; renamed functions, variables; fixed logic of programm

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,49 +1,38 @@
-import game from "../index.js";
+import playGame from "../index.js";
+import getRandomInteger from "../util.js";
 
-const description = "Find the greatest common divisor of given numbers.";
-
-const randomNum = () => Math.round(Math.random() * 100);
+const description = "What number is missing in the progression?";
 
 const getProgressionArray = () => {
   const progressionArray = [];
-  const term = Math.round(Math.random() * 10);
+  const term = getRandomInteger(1, 9);
 
-  const random = randomNum();
+  const firstElOfProgression = getRandomInteger(1, 9);
+  progressionArray[0] = firstElOfProgression;
 
   for (let i = 1; i < 10; i += 1) {
-    progressionArray[0] = random;
     progressionArray.push(progressionArray[i - 1] + term);
   }
 
-  const getNumberHiding = () =>
-    Math.round(Math.random() * progressionArray.length);
-
-  const numberHiding = getNumberHiding();
-
-  progressionArray[numberHiding] = "..";
   return progressionArray;
 };
 
-const getQuestionAndAnswer = () => {
-  const question = getProgressionArray();
-  let currentAnswer;
+const getIndexOfHiddenNumber = () => getRandomInteger(0, getProgressionArray().length - 1);
 
-  const index = question.indexOf("..");
-  if (index === question.length - 1 || index === question.length - 2) {
-    const diff = question[1] - question[0];
-    currentAnswer = String(diff * index + question[0]);
-  } else if (index === question[0] || index === question[1]) {
-    const diff = question[question.length - 1] - question[question.length - 2];
-    currentAnswer = String(question[question.length - 1] - diff * index);
-  } else {
-    currentAnswer = String(
-      question[index - 1] + (question[index + 1] - question[index - 1]) / 2
-    );
-  }
+const getQuestionAndAnswer = () => {
+  let progression = getProgressionArray();
+ 
+  const IndexOfHiddenNumber = getIndexOfHiddenNumber();
+  const hiddenNumber = progression[IndexOfHiddenNumber];
+  progression[IndexOfHiddenNumber] = "..";
+
+  let question = progression.toString().split(',').join(' ');
+
+  const currentAnswer = String(hiddenNumber);
 
   return [question, currentAnswer];
 };
 
 export default () => {
-  game(description, getQuestionAndAnswer);
+  playGame(description, getQuestionAndAnswer);
 };
