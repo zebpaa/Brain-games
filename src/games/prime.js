a865ce9119c49c0bd17a1d6fab2d76ab4c1f2cfc
zebpaa <skvorcov_90@mail.ru>
2024-06-22 14:43:32 +0400

    simplified the isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,20 +4,14 @@ import getRandomInteger from '../util.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  let flag = 0;
-  for (let i = 2; i < Math.sqrt(num) + 1; i += 1) {
-    if (num === 2) {
-      return 1;
-    }
-    if (num % i === 0) {
-      flag += 1;
-    }
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) return false;
   }
-  return flag > 0 ? 0 : 1;
+  return num > 1;
 };
 
 const getQuestionAndAnswer = () => {
-  const random = getRandomInteger(2, 9);
+  const random = getRandomInteger(1, 9);
   const question = random;
   const currentAnswer = isPrime(random) ? 'yes' : 'no';
 
